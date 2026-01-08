70d72faaa825824c127705565d3b5986229ad3e3
Dineshumar Periyasamy <dineshkumarperiyasamy11@gmail.com>
2026-01-08 10:51:13 +0530

    Create 05.driver.sv

---
+++ Full Adder/05.driver.sv
@@ -0,0 +1,22 @@
+class driver;
+  virtual intf vif;
+   mailbox gen2drv;
+  
+  function new(virtual intf vif,mailbox gen2drv);
+    this.vif = vif;
+    this.gen2drv = gen2drv;
+  endfunction
+  
+  task main();
+    repeat(12)
+    begin
+    transaction trans;
+    gen2drv.get(trans);
+    vif.a <= trans.a;
+    vif.b <= trans.b;
+    vif.c <= trans.c;
+    #1;
+      trans.display("Driver class signals");
+    end
+  endtask
+endclass
